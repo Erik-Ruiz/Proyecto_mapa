4433fd444b81bdcadfd74d119cde41763647d769
Uíliam Mateu <100007488.joan23@fje.edu>
2023-03-07 19:24:47 +0100

    Datos puntos mapas
    Mapas

---
+++ precharge.sql
@@ -0,0 +1,12 @@
+/* Inputs de "Puntos" */
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Estació Barceloneta', 'La estació de Barceloneta', '41.3835463,2.1882684', '0', NULL);
+
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Basílica de Santa Maria del Mar
+', 'Iglesia gótica del siglo XIV con altísimas columnas, 3 naves y grandes vidrieras.', '41.3834073,2.1864008', '0', NULL);
+
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Museo del Chocolate', 'Museo con obras de arte famosas elaboradas con chocolate, además de talleres para niños y adultos.', '41.3839708,2.1854567', '0', NULL);
+
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Paseo de Lluís Companys', 'Paseo de Lluís Companys', '41.387356,2.1773016', '0', NULL);
+
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Arco de Triunfo de Barcelona
+', 'Arco clásico construido como entrada principal a la Exposición Universal que se celebró en la ciudad en 1888.', '41.3910524,2.1765959', '0', NULL);
