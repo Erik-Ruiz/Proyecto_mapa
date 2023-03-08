79ef8bd6015fef8edaee9d251bede948f0eeef3b
Erik <78864402+Erik-Ruiz@users.noreply.github.com>
2023-03-08 18:38:07 +0100

    listado de puntos de interes

--- resources/js/mapa_principal.js
+++ resources/js/mapa_principal.js
@@ -4,6 +4,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var csrf_token = token.content;
 
 
 
@@ -12,15 +13,34 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function filtrar() {
     var ajax = new XMLHttpRequest();
+
     let formdata = new FormData;
+    formdata.append("_token", csrf_token);
 
     ajax.open('POST', "filtro_mapa_principal");
 
     ajax.onload = function() {
+        // console.log(ajax.responseText);
         data = JSON.parse(ajax.responseText)
-        console.log(ajax.responseText);
 
+        layerGroup.clearLayers();
+        try {
+
+            for (let index = 0; index < data.length; index++) {
+                const element = data[index];
+                console.log(element);
+
+               var mymarker = L.marker([element.coordenadas.split(",")[0], element.coordenadas.split(",")[1]]).addTo(layerGroup);
+               console.log(element.coordenadas.split(",")[0]+'..'+'..'+element.coordenadas.split(",")[1]);
+
+                mymarker.bindPopup("<b>" + element.nombre + "</b>");
+
+            }
+        } catch (e) {
+            console.log(e);
+        }
     }
     ajax.send(formdata);
 
 }
+filtrar('');
