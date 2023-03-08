70fb21c465850d04a7aad306aeede382d93d1e22
Uíliam Mateu <100007488.joan23@fje.edu>
2023-03-08 17:08:14 +0100

    actualización

--- precharge.sql
+++ precharge.sql
@@ -1,12 +1,7 @@
 /* Inputs de "Puntos" */
-INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Estació Barceloneta', 'La estació de Barceloneta', '41.3835463,2.1882684', '0', NULL);
-
-INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Basílica de Santa Maria del Mar
-', 'Iglesia gótica del siglo XIV con altísimas columnas, 3 naves y grandes vidrieras.', '41.3834073,2.1864008', '0', NULL);
-
-INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Museo del Chocolate', 'Museo con obras de arte famosas elaboradas con chocolate, además de talleres para niños y adultos.', '41.3839708,2.1854567', '0', NULL);
-
-INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Paseo de Lluís Companys', 'Paseo de Lluís Companys', '41.387356,2.1773016', '0', NULL);
-
-INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES (NULL, 'Arco de Triunfo de Barcelona
-', 'Arco clásico construido como entrada principal a la Exposición Universal que se celebró en la ciudad en 1888.', '41.3910524,2.1765959', '0', NULL);
+INSERT INTO `puntos` (`id`, `nombre`, `descripcion`, `coordenadas`, `personal`, `usuario`) VALUES
+(1, 'Estació Barceloneta', 'La estació de Barceloneta', '41.3835463,2.1882684', 0, NULL),
+(2, 'Basílica de Santa Maria del Mar\r\n', 'Iglesia gótica del siglo XIV con altísimas columnas, 3 naves y grandes vidrieras.', '41.3834073,2.1864008', 0, NULL),
+(3, 'Museo del Chocolate', 'Museo con obras de arte famosas elaboradas con chocolate, además de talleres para niños y adultos.', '41.3839708,2.1854567', 0, NULL),
+(4, 'Paseo de Lluís Companys', 'Paseo de Lluís Companys', '41.387356,2.1773016', 0, NULL),
+(5, 'Arco de Triunfo de Barcelona\r\n', 'Arco clásico construido como entrada principal a la Exposición Universal que se celebró en la ciudad en 1888.', '41.3910524,2.1765959', 0, NULL);
