5264b051de20a5fd7c3aaa2b1cf1ac4bab77173f
Uíliam Mateu <100007488.joan23@fje.edu>
2023-03-09 16:00:19 +0100

    09

--- resources/js/mapa_principal.js
+++ resources/js/mapa_principal.js
@@ -39,23 +39,16 @@ function filtrar() {
 
         layerGroup.clearLayers();
         try {
-            if (filtro_etiqueta.value == 'NO') {
-                for (let index = 0; index < data.length; index++) {
-                    const element = data[index];
-                    var mymarker = L.marker([element.coordenadas.split(",")[0], element.coordenadas.split(",")[1]]).addTo(layerGroup);
 
-                    mymarker.bindPopup("<b>" + element.nombre + "</b>");
+            for (let index = 0; index < data.length; index++) {
+                const element = data[index];
+                var mymarker = L.marker([element.latitud, element.longitud]).addTo(layerGroup);
 
-                }
-            } else {
-                for (let index = 0; index < data.length; index++) {
-                    const element = data[index];
-                    for (let i = 0; i < element.length; i++) {
-                        var mymarker = L.marker([element[i].coordenadas.split(",")[0], element[i].coordenadas.split(",")[1]]).addTo(layerGroup);
-                        mymarker.bindPopup("<b>" + element[i].nombre + "</b>");
-                    }
-                }
+                mymarker.bindPopup("<b>" + element.nombre + "</b>");
+
             }
+
+
 
         } catch (e) {
             console.log(e);
