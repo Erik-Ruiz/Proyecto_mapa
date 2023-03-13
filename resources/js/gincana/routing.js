cad0d4b17fe157de3b29528387622a30d7d67b3d
Zacoleo <jordi.cort02@gmail.com>
2023-03-13 15:32:58 +0100

    Push con routing

--- resources/js/gincana/routing.js
+++ resources/js/gincana/routing.js
@@ -1,4 +1,7 @@
 var map = L.map('map');
+var latitud = 200;
+var longitud = 200;
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
@@ -12,12 +15,14 @@ function getPuntosInteres(){
         var data = JSON.parse(ajax.responseText);
 
         for (let index = 0; index < data.length; index++) {
+
             const element = data[index];
             var mymarker = L.marker([(element.latitud), (element.longitud)]).addTo(map);
 
             mymarker.bindPopup("<b>" + element.nombre + "</b>");
-
         }
+
+        document.getElementsByClassName("leaflet-routing-container")[0].style.display = "none";
 
     }
     ajax.send();
@@ -26,10 +31,27 @@ function getPuntosInteres(){
 L.Routing.control({
     waypoints: [
         L.latLng(41.38211, 2.18548) ,
-        L.latLng(41.38458, 2.18128)
+        L.latLng(41.38211, 2.18548)
     ],
     routeWhileDragging: true
+    
 }).addTo(map);
+
+//#endregion
+
+map.on("click", function (e) {
+   
+    L.Routing.control({
+        waypoints: [
+            L.latLng((e.latlng.lat), (e.latlng.lng)),
+            L.latLng(41.38458, 2.18128)
+        ],
+        routeWhileDragging: true
+    }).addTo(map);
+    
+    console.log(map);
+});
 
 getPuntosInteres();
 
+//#region no abrir
